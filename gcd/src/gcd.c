b7801c66c232d8ee9b0e63366ab993fd44648d52
AngelVerras <angelverras2006@gmail.com>
2024-12-05 20:06:53 +0200

    Update gcd.c

--- gcd/src/gcd.c
+++ gcd/src/gcd.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
 
+int num0,num1; // Αρχικοποίηση Μεταβλητών
+
+int gcd(int a, int b){ // Συνάρτηση ΜΚΔ Ευκλίδη
+
+  if(b==0){
+    return a;
+  }  else{
+        return gcd(b, a%b);
+  }
+}
+
+int main(){ //Κύρια Συνάρτηση
+
+    printf("Give the first number: "); // Εισαγωγή πρώτου αριθμού
+    scanf("%d", &num0);
+
+    printf("Give the second number: "); // Εισαγωγή δεύτερου αριθμού
+    scanf("%d", &num1);
+
+    int sum = gcd(num0,num1); // Υπολογισμός συνάρτησης
+
+    printf("The GCD of %d and %d is: %d\n", num0, num1, sum); // Εμφάνιση αποτελέσματος συνάρτησης
+
+    return 0;
+
+
+}
